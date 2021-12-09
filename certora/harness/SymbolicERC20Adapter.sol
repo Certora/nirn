d9497a57ef527fc1f2bee6f4acf6a15ff44fa9b8
Nurit Dor <nurit@certora.com>
2021-12-09 21:20:15 +0200

    fix adapter and some rules

--- certora/harness/SymbolicERC20Adapter.sol
+++ certora/harness/SymbolicERC20Adapter.sol
@@ -11,13 +11,13 @@ contract SymbolicERC20Adapter {
   string _name;
   
   // to be forced as nondet
-  uint _availableLiquidityDummy;
-  uint _toUnderlyingAmountDummy;
-  uint _toWrappedAmountDummy;
+  //uint _availableLiquidityDummy;
+  //uint _toUnderlyingAmountDummy;
+  //uint _toWrappedAmountDummy;
   uint _aprDummy;
   uint _hypotheticalAprDummy;
-  uint _balanceWrappedDummy; // probably can connect to something
-  uint _balanceUnderlyingDummy; // probably can connect to something
+  //uint _balanceWrappedDummy; // probably can connect to something
+  //uint _balanceUnderlyingDummy; // probably can connect to something
 
   function underlying() external view returns (address) { return _underlying; }
 
@@ -25,13 +25,13 @@ contract SymbolicERC20Adapter {
 
   function name() external view returns (string memory) { return _name; }
 
-  function availableLiquidity() external view returns (uint256) { return _availableLiquidityDummy; }
+  function availableLiquidity() external view returns (uint256) { return IERC20(_underlying).balanceOf(address(this)); }
 
 /* ========== Conversion ========== */
 
-  function toUnderlyingAmount(uint256 tokenAmount) external view returns (uint256) { return _toUnderlyingAmountDummy; }
+  function toUnderlyingAmount(uint256 tokenAmount) external view returns (uint256) { return tokenAmount; }
 
-  function toWrappedAmount(uint256 underlyingAmount) external view returns (uint256) { return _toWrappedAmountDummy; }
+  function toWrappedAmount(uint256 underlyingAmount) external view returns (uint256) { return underlyingAmount; }
 
 /* ========== Performance Queries ========== */
 
@@ -50,65 +50,76 @@ contract SymbolicERC20Adapter {
 
 /* ========== Caller Balance Queries ========== */
 
-  function _balanceWrapped() private view returns (uint256) { return _balanceWrappedDummy; }
+  function _balanceWrapped() private view returns (uint256) { return IERC20(_underlying).balanceOf(address(this)); }
   function balanceWrapped() external view returns (uint256) { return _balanceWrapped(); }
 
-  function balanceUnderlying() external view returns (uint256) { return _balanceUnderlyingDummy; }
+  function balanceUnderlying() external view returns (uint256) { return IERC20(_underlying).balanceOf(address(this));}
 
-  function setBalanceUnderlying(uint256 newBalance) external { _balanceUnderlyingDummy = newBalance; }
+  address public externalUserAddress;
+  function setBalanceUnderlying(uint256 newBalance) external { 
+      require(externalUserAddress != msg.sender && externalUserAddress != address(this),"1");
+      uint256 currentBalance = _balanceWrapped();
+      if (currentBalance > newBalance) 
+        IERC20(_underlying).transfer(externalUserAddress, currentBalance - newBalance);
+      else
+         IERC20(_underlying).transferFrom(externalUserAddress, address(this), newBalance - currentBalance);
+  }
 
 /* ========== Interactions ========== */
 
-  mapping (uint => mapping (uint => uint)) underlyingToAmount;
+ /* mapping (uint => mapping (uint => uint)) underlyingToAmount;
   mapping (uint => mapping (uint => uint)) amountToUnderlying;
   uint exchangeMapIdx;
 
   bool doesSelfTrack;
   mapping (address => uint) mintTracking;
-
+*/
   function deposit(uint256 amountUnderlying) external returns (uint256 amountMinted) {
-      amountMinted = underlyingToAmount[exchangeMapIdx++][amountUnderlying];
+      //amountMinted = underlyingToAmount[exchangeMapIdx++][amountUnderlying];
       IERC20(_underlying).transferFrom(msg.sender, address(this), amountUnderlying);
-      if (doesSelfTrack) {
+      /*if (doesSelfTrack) {
           mintTracking[msg.sender] = mintTracking[msg.sender].add(amountMinted);
-      }
+      }*/
   }
 
   function _withdraw(uint256 amountToken) private returns (uint256 amountReceived) {
-      amountReceived = amountToUnderlying[exchangeMapIdx++][amountToken];
+      //amountReceived = amountToUnderlying[exchangeMapIdx++][amountToken];
       IERC20(_underlying).transfer(msg.sender, amountReceived);
-      if (doesSelfTrack) {
+      return amountReceived;
+      /*if (doesSelfTrack) {
           mintTracking[msg.sender] = mintTracking[msg.sender].sub(amountToken);
-      }
+      }*/
   }
   function withdraw(uint256 amountToken) external returns (uint256 amountReceived) {
-      _withdraw(amountToken);
+      return _withdraw(amountToken);
   }
 
-  function withdrawAll() external returns (uint256 amountReceived) {
-    if (!doesSelfTrack) { 
+  function withdrawAll() external returns (uint256 total) {
+    /*if (!doesSelfTrack) { 
         _withdraw(_balanceWrapped());
     } else { 
         _withdraw(mintTracking[msg.sender]);
-    }
+    }*/
+    
+    return _withdraw(_balanceWrapped());
   }
 
   function withdrawUnderlying(uint256 amountUnderlying) external returns (uint256 amountBurned) {
-        amountBurned = underlyingToAmount[exchangeMapIdx++][amountUnderlying];
-        _withdraw(amountBurned);
+        //amountBurned = underlyingToAmount[exchangeMapIdx++][amountUnderlying];
+        _withdraw(amountUnderlying);
   }
 
   function withdrawUnderlyingUpTo(uint256 amountUnderlying) external returns (uint256 amountReceived) {
-      amountReceived = underlyingToAmount[exchangeMapIdx++][amountUnderlying];
-      uint bal;
-      if (!doesSelfTrack) {
-          bal = _balanceWrapped();         
+      amountReceived = amountUnderlying; //underlyingToAmount[exchangeMapIdx++][amountUnderlying];
+      uint bal = _balanceWrapped();
+      /*if (!doesSelfTrack) {
+          bal = ;         
       } else {
           bal = mintTracking[msg.sender];
       }
-
+      */
       uint min = amountReceived > bal ? bal : amountReceived;
 
-      _withdraw(min);
+      return _withdraw(min);
   }
 }
